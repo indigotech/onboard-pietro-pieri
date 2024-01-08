b8eaab38a865651bbac476bf4d64bed60c14e3fa
Pietro Pieri <pietro.pieri@taqtile.com.br>
2024-01-08 15:54:20 -0300

    Use validade login function in loginScreen

--- OnboardPietroPieri/App.tsx
+++ OnboardPietroPieri/App.tsx
@@ -5,28 +5,53 @@
  * @format
  */
 
-import React from "react";
-import {
-  SafeAreaView,
-  View,
-  Text,
-  StyleSheet
-} from 'react-native';
+import React, { useState } from "react";
+import { Alert, SafeAreaView, View, Text, StyleSheet } from "react-native";
 import {LoginButton, TextField} from './components/login-screen'
 
 const App = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const isValidEmail = (email: string) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const isValidPassword = (password: string) => {
+    return (
+      password.length >= 7 && /[a-zA-Z]/.test(password) && /\d/.test(password)
+    );
+  };
+
+  const validateLogin = () => {
+    if (!isValidEmail(email)) {
+      Alert.alert("Erro", "Por favor, insira um email válido.");
+      return;
+    }
+
+    if (!isValidPassword(password)) {
+      Alert.alert(
+        "Erro",
+        "Por favor, a senha deve não ser vazia, deve ter pelo menos 7 caracteres, pelo menos uma letra e um dígito.",
+      );
+      return;
+    }
+
+    Alert.alert("Sucesso!", `Bem-vindo, ${email}!`);
+  };
+
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.center}>
         <Text style={styles.header}>Bem vindo(a) à Taqtile!</Text>
       </View>
 
-      <TextField title="E-mail" />
+      <TextField title="E-mail" onChange={setEmail} />
 
-      <TextField title="Senha" type="password"/>
+      <TextField title="Senha" type="password" onChange={setPassword} />
 
-      <LoginButton body="Entrar" />
-
+      <LoginButton body="Entrar" onPress={validateLogin} />
     </SafeAreaView>
   );
 };
@@ -38,18 +63,17 @@ const styles = StyleSheet.create({
   },
   center: {
     marginTop: 30,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     flex: 0.3,
     paddingHorizontal: 24,
   },
   header: {
     fontSize: 30,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: 'black',
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "black",
   },
 });
-
 
 export default App;
