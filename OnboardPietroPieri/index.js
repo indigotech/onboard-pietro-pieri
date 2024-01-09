c53dfaaa086399a1e469e58105fee241a9d02f59
Pietro Pieri <pietro.pieri@taqtile.com.br>
2024-01-09 12:07:01 -0300

    New functionality in the App

--- OnboardPietroPieri/index.js
+++ OnboardPietroPieri/index.js
@@ -2,8 +2,17 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from "react-native";
+import App from "./App";
+import { name as appName } from "./app.json";
+import client from "./apollo/apolloclient";
+import { ApolloProvider } from "@apollo/client";
+import React from "react";
 
-AppRegistry.registerComponent(appName, () => App);
+const AppApollo = () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
+
+AppRegistry.registerComponent(appName, () => AppApollo);
