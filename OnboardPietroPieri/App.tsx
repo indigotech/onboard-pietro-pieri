5f28192dedeaa5f5e474be2ada7b048da36450df
Pietro Pieri <pietro.pieri@taqtile.com.br>
2024-01-10 15:10:36 -0300

    App navegation

--- OnboardPietroPieri/App.tsx
+++ OnboardPietroPieri/App.tsx
@@ -5,62 +5,32 @@
  * @format
  */
 
-import React, { useState } from "react";
-import { Alert, SafeAreaView, View, Text, StyleSheet } from "react-native";
-import { LoginButton, TextField } from "./components/login-screen";
-import { isValidEmail, isValidPassword } from "./validation/validation";
-import { LOGIN } from "./apollo/mutation";
-import { useMutation } from "@apollo/client";
-import { LoginData } from "./interfaces/mutation";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import React from "react";
+import { SafeAreaView, StyleSheet } from "react-native";
+import { LoginPage } from "./components/login-page";
+import { client } from "./apollo/apolloclient";
+import { ApolloProvider } from "@apollo/client";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { BlankPage } from "./components/blank-page";
+
+const Stack = createStackNavigator();
 
 const App = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [loginMutation, { error, data }] = useMutation<LoginData>(LOGIN);
-
-  const validateLogin = async () => {
-    if (!isValidEmail(email)) {
-      Alert.alert("Erro", "Por favor, insira um email válido.");
-      return;
-    }
-
-    if (!isValidPassword(password)) {
-      Alert.alert(
-        "Erro",
-        "Por favor, a senha deve não ser vazia, deve ter pelo menos 7 caracteres, pelo menos uma letra e um dígito.",
-      );
-      return;
-    }
-
-    try {
-      const response = await loginMutation({
-        variables: { data: { email: email, password: password } },
-      });
-
-      if (response.data) {
-        await AsyncStorage.setItem("userToken", response.data.login.token);
-      }
-
-      Alert.alert("Sucesso!", `Bem-vindo, ${email}!`);
-    } catch (error) {
-      console.log(error);
-      Alert.alert("Erro", "Ocorreu um erro durante o login.");
-    }
-  };
-
   return (
-    <SafeAreaView style={styles.safeArea}>
-      <View style={styles.center}>
-        <Text style={styles.header}>Bem vindo(a) à Taqtile!</Text>
-      </View>
-
-      <TextField title="E-mail" onChange={setEmail} />
-
-      <TextField title="Senha" type="password" onChange={setPassword} />
-
-      <LoginButton body="Entrar" onPress={validateLogin} />
-    </SafeAreaView>
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <SafeAreaView style={styles.safeArea}>
+          <Stack.Navigator
+            initialRouteName="LoginPage"
+            screenOptions={{ headerShown: false }}
+          >
+            <Stack.Screen name="LoginPage" component={LoginPage} />
+            <Stack.Screen name="BlankPage" component={BlankPage} />
+          </Stack.Navigator>
+        </SafeAreaView>
+      </NavigationContainer>
+    </ApolloProvider>
   );
 };
 
@@ -68,19 +38,6 @@ const styles = StyleSheet.create({
   safeArea: {
     marginTop: 80,
     flex: 1,
-  },
-  center: {
-    marginTop: 30,
-    alignItems: "center",
-    justifyContent: "center",
-    flex: 0.3,
-    paddingHorizontal: 24,
-  },
-  header: {
-    fontSize: 30,
-    fontWeight: "bold",
-    textAlign: "center",
-    color: "black",
   },
 });
 
--- OnboardPietroPieri/index.js
+++ OnboardPietroPieri/index.js
@@ -1,18 +1,9 @@
 /**
  * @format
  */
-
+import "react-native-gesture-handler";
 import { AppRegistry } from "react-native";
 import App from "./App";
 import { name as appName } from "./app.json";
-import { client } from "./apollo/apolloclient";
-import { ApolloProvider } from "@apollo/client";
-import React from "react";
 
-const AppApollo = () => (
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>
-);
-
-AppRegistry.registerComponent(appName, () => AppApollo);
+AppRegistry.registerComponent(appName, () => App);
