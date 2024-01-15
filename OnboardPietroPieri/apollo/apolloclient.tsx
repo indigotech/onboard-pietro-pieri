3fa3a5cecfea4f8d7df71d4920ec16003e6c1758
Pietro Pieri <pietro.pieri@taqtile.com.br>
2024-01-15 16:52:48 -0300

    Use token in apollo client

--- OnboardPietroPieri/apollo/apolloclient.tsx
+++ OnboardPietroPieri/apollo/apolloclient.tsx
@@ -1,6 +1,22 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const httpLink = createHttpLink({
+  uri: "https://template-onboarding-node-sjz6wnaoia-uc.a.run.app/graphql",
+});
+
+const authLink = setContext(async (_, { headers }) => {
+  const token = await AsyncStorage.getItem("userToken");
+  return {
+    headers: {
+      ...headers,
+      authorization: token,
+    },
+  };
+});
 
 export const client = new ApolloClient({
-  uri: "https://template-onboarding-node-sjz6wnaoia-uc.a.run.app/graphql",
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
